bb4dee2c3334cc4842619f30aaf6d55b7398fdfa
itclub-batu <146393612+itclub-batu@users.noreply.github.com>
2023-11-15 01:24:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ window.onload = () => {
         duration: 1000,
     });
 }
+
+// Function to show an alert when a button is clicked
+function showAlert() {
+    // Select all elements with class "btn-incomplete"
+    var btnIncompleteList = document.querySelectorAll(".btn-incomplete");
+
+    // Select all elements with class "notification"
+    var notificationList = document.querySelectorAll(".notification");
+
+    // Iterate over each button in the list
+    btnIncompleteList.forEach(function(btnIncomplete, index) {
+        // Create a new alert element
+        var alertDiv = document.createElement("div");
+
+        // Set the class for styling
+        alertDiv.className = "alert alert-primary text-center";
+
+        // Set the ARIA role attribute for accessibility
+        alertDiv.setAttribute("role", "alert");
+
+        // Set the text content of the alert
+        alertDiv.textContent = "Coming soon";
+
+        // Append the alert element to the corresponding notification element
+        notificationList[index].appendChild(alertDiv);
+    });
+}
