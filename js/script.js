eb6581d9ab40f71cd3daf78e8ecf51672900f543
itclub-batu <146393612+itclub-batu@users.noreply.github.com>
2023-11-14 19:11:19 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// This JavaScript code initializes the AOS (Animate On Scroll) library when the window has finished loading.
+window.onload = () => {
+    // Initialize AOS with a duration of 1000 milliseconds for smooth animations.
+    AOS.init({
+        duration: 1000,
+    });
+}
